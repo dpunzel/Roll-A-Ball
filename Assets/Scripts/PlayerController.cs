a19bcc079ce7ad195f384726be2c931c289d92c3
DAVID <dpunzel15@georgefox.edu>
2017-10-29 21:53:17 -0700

    Inital Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float speed = 800.0f;
+	public Text scoreText;
+	public Text winText;
+	private int count = 0;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count += 1;
+			scoreText.text = "Score: " + count;
+		}
+
+
+		if (count >= 16)
+		{
+			winText.gameObject.SetActive(true);
+		}
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/TimerCountDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerCountDown : MonoBehaviour {
+	float timeRemaining = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+	}
+	void OnGUI(){
+		if (timeRemaining > 0) {
+			GUI.Label (new Rect (100, 100, 200, 100), "Time Remaining :" + timeRemaining);
+		} else {
+			GUI.Label (new Rect (100, 100, 100, 100), "Time's up! ");
+		}
+	}
+}
